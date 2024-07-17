3f8e5e3d072356aa2546068e745d37598f5bdb11
Shivangi <shivangitrivedi2021@gmail.com>
2024-07-17 12:16:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("./conn/conn");
+const User = require("./routes/user");
+const Books = require("./routes/book");
+const Favourite = require("./routes/favourite");
+const Cart = require("./routes/cart")
+const Order = require("./routes/order")
+app.use(express.json());
+
+app.use("/api/v1" , User)
+app.use("/api/v1" , Books)
+app.use("/api/v1" , Favourite)
+app.use("/api/v1" , Cart)
+app.use("/api/v1" , Order)
+app.listen(process.env.PORT,()=>{
+    console.log(`Server started at port ${process.env.Port}`);
+});
---
+++ models/order.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const order = new mongoose.Schema({
+    user:{
+        type:mongoose.Types.ObjectId,
+            ref:"user",
+        
+    },
+book:{
+    type:mongoose.Types.ObjectId,
+    ref:"books",
+},
+status:{
+    type:String,
+    default:"order Placed",
+    enum:["order placed","our for delievery,delivered,cancelled"]
+},
+},
+{
+    timestamps:true
+});
+module.exports=mongoose.model("order",order);
